37e43b67e470d4babc203752805e8970877c8f9b
성호  권 <maystar0503@naver.com>
2023-02-03 18:16:32 +0900

    [Gold III] Title: 앱, Time: 152 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,32 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 	static int[] cache;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		int[] memory = new int[n + 1];
 		int[] cost = new int[n + 1];
 		int size = 0;
-
+		
+		// 메모리 파싱
+		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i < memory.length; i++)
-			memory[i] = sc.nextInt();
-
+			memory[i] = Integer.parseInt(st.nextToken());
+		// 비용 파싱
+		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i < cost.length; i++) {
-			cost[i] = sc.nextInt();
+			cost[i] = Integer.parseInt(st.nextToken());
 			size += cost[i];
 		}
 
-		// 해당 코스트로 이용할 수 있는 최대 메모리를 계산하는 메모이제이션 배열
+		// 해당 cost로 이용할 수 있는 최대 메모리를 계산하는 dp 배열
 		cache = new int[size + 1];
-
+		
+		// i 번째 앱까지 탐색했을 때 j 비용으로 // cashe는 메모리 값
 		for (int i = 0; i < cost.length; i++)
 			for (int j = size; j >= cost[i]; j--)
+				//j 만큼의 비용으로 확보할 수 있는 최대 메모리를 계산
+				// cache[j] : j 비용 써서 확보한 메모리 (현재 i 번째 앱을 킨 경우)
+				// cache[j - cost[i]] + memory[i] : i번째 앱 비활성화
 				cache[j] = Math.max(cache[j], cache[j - cost[i]] + memory[i]);
 
-		// 코스트 중에 처음으로 m이상의 최대 메모리를 갖는 코스트를 출력
+		// cost 중에 처음으로 m이상의 최대 메모리를 갖는 코스트를 출력
 		for (int i = 0; i < cache.length; i++)
 			if (cache[i] >= m) {
 				System.out.println(i);
