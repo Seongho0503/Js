e77db3a78690f3d5ba6e397b581e61e6fb69fa26
lea_hwang <37495515+lea-hwang@users.noreply.github.com>
2023-02-06 00:04:56 +0900

    Update and rename 3진법 뒤집기.js to 3진법 뒤집기_희원.js

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기_희원.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기_희원.js
@@ -1,6 +1,5 @@
 function solution(n) {
-    var answer = 0;
-    
+    // 제일 첫번째 숫자부터 3^0, 3^1, 3^2, ...를 곱하면서 더해준다. 
     const num3 = [...n.toString(3)].reduce((acc, cur, curidx) => {
         return acc + cur * 3 ** curidx;
     }, 0)
