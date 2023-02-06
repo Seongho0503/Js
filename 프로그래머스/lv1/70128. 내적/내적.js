b44db7d9dc56df5a779776c8120c520a554e7045
성호  권 <maystar0503@naver.com>
2023-02-06 22:40:47 +0900

    [level 1] Title: 내적, Time: 12.57 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    var sum = 0;
+    a.map((a, idx1) => {
+        b.map((b, idx2)=>{
+            if(idx1===idx2) sum = sum + (a * b);
+        })
+    })
+    return sum;
+}
