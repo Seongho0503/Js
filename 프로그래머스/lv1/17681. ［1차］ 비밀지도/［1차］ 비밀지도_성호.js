577649b860c59847d13f583924e92c8287b990e8
성호  권 <maystar0503@naver.com>
2023-02-07 18:40:49 +0900

    Rename ［1차］ 비밀지도.js to ［1차］ 비밀지도_성호.js (#6)

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도_성호.js
@@ -0,0 +1,48 @@
+function solution(n, arr1, arr2) {
+    var answer = [];
+    arr1.map((id1, idx1)=>{
+        arr2.map((id2, idx2)=>{{
+            if(idx1 === idx2){
+              var str1 = id1.toString(2).padStart(n, '0');
+              var str2 = id2.toString(2).padStart(n, '0');
+              str = "";
+              for(var i = 0; i<n; i++){
+                if(str1.charAt(i) + str2.charAt(i) ==="00") str += " ";
+                else str += "#"; 
+              }
+              answer[idx2] =str;
+            }
+        }})
+    })
+    
+    return answer;
+}
+
+// 둘중 하나라도 벽 => 벽
+// 모두 공백 => 공백
+
+// function solution(n, arr1, arr2) {
+//     var answer = [];
+//     // const zeroFilter = (st1, st2) =>{
+//     //     str1 = st1.padStart(5, '0');
+//     //     str2 = st2.padStart(5, '0');
+//     //     //for(let i = 0; i <n-st.length; i++) st.charAt(i) 
+//     // }
+//     arr1.map((id1, idx1)=>{
+//         arr2.map((id2, idx2)=>{{
+//             if(idx1 === idx2){
+//               var str1 = id1.toString(2).padStart(5, '0');
+//               var str2 = id2.toString(2).padStart(5, '0');
+//               //zeroFilter(str1, str2);
+//               str = "";
+//               for(var i = 0; i<n; i++){
+//                 if(str1.charAt(i) + str2.charAt(i) ==="00") str += " ";
+//                 else str += "#"; 
+//               }
+//               answer[idx2] =str;
+//             }
+//         }})
+//     })
+    
+//     return answer;
+// }
