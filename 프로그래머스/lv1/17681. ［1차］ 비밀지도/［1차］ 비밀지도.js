b1afc9036bb3390fa3289d1dbbec5a326291e691
lea_hwang <37495515+lea-hwang@users.noreply.github.com>
2023-02-07 17:17:06 +0900

    Update ［1차］ 비밀지도.js
    == 를 ===로 고쳤습니다!
    https://developer.mozilla.org/ko/docs/Web/JavaScript/Equality_comparisons_and_sameness

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -12,7 +12,7 @@ function solution(n, arr1, arr2) {
         let line = "";
         
         for (let j = 0; j < n; j++){
-            line += num1[j] == 1 || num2[j] == 1 ? "#" : " ";
+            line += num1[j] === 1 || num2[j] === 1 ? "#" : " ";
         }
         answer.push(line);
     }
