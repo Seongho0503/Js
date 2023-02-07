75df3938c98bd48144a51fc4c05e3c8ec41d9fdd
성호  권 <maystar0503@naver.com>
2023-02-07 18:28:48 +0900

    Update and rename ［1차］ 비밀지도.js to ［1차］ 비밀지도_성호.js

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도_성호.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도_성호.js
@@ -1,3 +1,22 @@
+// 코드 리뷰 수정ver
+
+function solution(n, arr1, arr2) {
+    var answer = [];
+    arr1.map((id1, idx1)=>{
+              var str1 = id1.toString(2).padStart(n, '0');
+              var str2 = arr2[idx1].toString(2).padStart(n, '0');
+              str = "";
+              for(var i = 0; i<n; i++){
+                if(str1.charAt(i) + str2.charAt(i) ==="00") str += " ";
+                else str += "#"; 
+              }
+              answer[idx1] =str;
+        })
+    return answer;
+}
+
+
+
 function solution(n, arr1, arr2) {
     var answer = [];
     arr1.map((id1, idx1)=>{
