a99c7edaa50552d6351b33cb0f14806bc2a1417b
성호  권 <maystar0503@naver.com>
2023-02-09 18:44:34 +0900

    Update 짝지어 제거하기_성호.js

--- 프로그래머스/lv1/12973. 짝지어 제거하기/짝지어 제거하기_성호.js
+++ 프로그래머스/lv1/12973. 짝지어 제거하기/짝지어 제거하기_성호.js
@@ -1,6 +1,6 @@
 function solution(s) {
-  var answer = 1;
-  var str = [];
+  let answer = 1;
+  let str = [];
   for (let i = 0; i < s.length; i++) {
     // 지금 인덱스의 문자열 값과 배열에 넣은 인덱스-1(이전 값)과 비교한다
     if (str[str.length - 1] === s[i]) str.pop();
